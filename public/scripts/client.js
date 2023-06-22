6d5fd514dc18fbd4f49c1856060779ac10b0d8b8
NFerracuti <npferracuti@gmail.com>
2023-06-22 16:17:23 -0400

    add dynamic styling for desktops vs mobile and tablet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -13,12 +13,17 @@ $(document).ready(function () {
     return div.innerHTML;
   };
 
- const createTweetElement = function (obj) {
+  //By default, error messages are hidden.
+  $("#error-mess-empty").hide();
+  $("#error-mess-tooLong").hide();
+
+  //Creates a new tweet element formatted with HTML to feed to the DOM
+  const createTweetElement = function (obj) {
     return `
       <article>
         <header>
-          <div>
-            <i>${obj.user.avatars}</i>${obj.user.name}
+          <div class="icon-name">
+          <img src="${escape(obj.user.avatars)}">${obj.user.name}
           </div>
           <div>
             ${obj.user.handle}
@@ -43,21 +48,36 @@ $(document).ready(function () {
     `;
   };
 
+  //Renders the tweet data by preending tweet html
   const renderTweets = (tweets) => {
     for (const tweet of tweets) {
       $('#tweets').prepend(createTweetElement(tweet))
     }
   }
 
+  //event handler for when user submits new tweet
   $("#new-tweet-form").submit((event) => {
     event.preventDefault();
 
     const newTweetBody = {text: event.target.text.value};
     const tweetText = newTweetBody.text;
     if (tweetText.length > 140) {
-      return alert("tweet too long, reduce to under 140 chars");
+      $("#error-mess-tooLong").slideDown("slow");
+      $("#error-mess-empty").hide();
+  
+      // Hide the error message after 2 seconds
+      setTimeout(function() {
+        $("#error-mess-tooLong").slideUp("slow");
+      }, 2000);
+  
     } else if (tweetText.length <= 0) {
-      return alert("tweet must have content");
+      $("#error-mess-empty").slideDown("slow");
+      $("#error-mess-tooLong").hide();
+  
+      // Hide the error message after 2 seconds
+      setTimeout(function() {
+        $("#error-mess-empty").slideUp("slow");
+      }, 2000);
     } else {
       $.post( "/tweets", newTweetBody, function() {
         console.log("tweet submitted successfully");
@@ -66,6 +86,7 @@ $(document).ready(function () {
     }
   })
 
+  //AJAX 'get' request to add the new tweet data to the tweets arrauy
   const loadTweets = function() {
     $.get("/tweets", (response) => {
       renderTweets(response);
